a6f41eec9d06b4a0e578ff9205da43d0881f5834
meld-cp <18450687+meld-cp@users.noreply.github.com>
2024-04-08 23:42:08 +1200

    wip

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFolder } from 'obsidian';
+import { exec } from 'child_process';
+import { App, FileSystemAdapter, Notice, Plugin, PluginManifest, PluginSettingTab, TFolder, normalizePath } from 'obsidian';
+import * as path from 'path';
 
 
 // TODO: add settings to configure eleventy.config.js (https://www.11ty.dev/docs/config/)
@@ -13,37 +15,76 @@ const DEFAULT_SETTINGS: Meld11tyPluginSettings = {
 export default class Meld11tyPlugin extends Plugin {
 	settings: Meld11tyPluginSettings;
 
+	constructor(app: App, manifest: PluginManifest) {
+		super(app, manifest);
+	}
+
 	async onload() {
 		await this.loadSettings();
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new Meld11tySettingTab(this.app, this));
 
-		this.registerEvent( this.app.workspace.on( 'file-menu', (menu, file, source) => {
-			if (!(file instanceof TFolder)){
+		this.registerEvent( this.app.workspace.on( 'file-menu', async (menu, folder, source) => {
+			if (!(folder instanceof TFolder)){
 				return;
 			}
 
-			menu.addItem((item) => {
-				item.setTitle('Build Static Site').onClick(async () => {
-					await this.buildStaticSite(file);
-				});
+			// check for 11ty config file
+			const configFilePath = normalizePath( path.join(folder.path, 'eleventy.config.js') );
+
+			//console.debug({configFilePath});
+			
+			menu.addItem(async (item) => {
+				const configFileExists = await this.app.vault.adapter.exists( configFilePath );
+
+				//const menuItemLabel = configFileExists ? 'Build Static Site' : 'Init Static Site';
+
+				if ( configFileExists ){
+					item
+						.setTitle('Build Static Site').onClick(async () => await this.buildStaticSite(folder) )
+					;
+				}else{
+					item
+						.setTitle('Init. as Static Site').onClick(async () => await this.initStaticSite(folder) )
+					;
+				}
 			});
+
 		}))
 
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	initStaticSite(folder: TFolder): any {
+		// TODO: build eleventy.config.js
 	}
 
 	async buildStaticSite(folder: TFolder) : Promise<void> {
-		//TODO: convert ![[images]] to <img src..>
-		new Notice( `Build Static Site: ${folder.path}` );
+		
+		if ( !(this.app.vault.adapter instanceof FileSystemAdapter) ) {
+			return;
+		}
+		
+		const basePath = this.app.vault.adapter.getBasePath();
+		const fullFolderPath = path.join( basePath, folder.path );
+
+		//console.debug( {basePath, fullFolderPath} );
+		
+
+		// run eleventy to build the static site
+		
+		new Notice( `Building Site: '${folder.path}'...` );
+
+		exec( 'npx @11ty/eleventy', { cwd: fullFolderPath }, (err, stdout, stderr) => {
+			if (err) {
+			  console.error(err);
+			  new Notice( `There was an error while building: '${folder.path}', see console` );
+			  return;
+			}
+			console.log(stdout);
+			new Notice( `Site: '${folder.path}' was built` );
+		  });
 	}
 
 	onunload() {
